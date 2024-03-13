660c686a81820802c12986fa0ddf0441d2fb2d57
Deepak <deepak988088@gmail.com>
2024-03-14 04:08:33 +0530

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const server = app.listen(5000, () => {
 // Attach Socket.IO to the HTTP server
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   }
 });
